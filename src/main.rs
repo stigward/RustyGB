f586f1a4f7312cd82e9a2c8ab2b3ed34160eea20
stigward <jmaginnes@gmail.com>
2023-01-14 15:18:58 -0700

    init commit

---
+++ src/main.rs
@@ -0,0 +1,71 @@
+struct Registers {
+    a: u8,
+    b: u8,
+    c: u8,
+    d: u8,
+    e: u8,
+    f: u8,
+    g: u8,
+    h: u8,
+    l: u8,
+}
+
+// f register 
+struct FlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+
+const ZERO_FLAG_BYTE_POSITION: u8 = 7;
+const SUBTRACT_FLAG_BYTE_POSITION: u8 = 6;
+const HALF_CARRY_FLAG_BYTE_POSITION: u8 = 5;
+const CARRY_FLAG_BYTE_POSITION: u8 = 4;
+
+// Use std library function to convert between FlagsRegister and u8. Bit shifting based on
+// constants defined above
+impl std::convert::From<FlagsRegister> for u8  {
+    fn from(flag: FlagsRegister) -> u8 {
+        (if flag.zero       { 1 } else { 0 }) << ZERO_FLAG_BYTE_POSITION |
+        (if flag.subtract   { 1 } else { 0 }) << SUBTRACT_FLAG_BYTE_POSITION |
+        (if flag.half_carry { 1 } else { 0 }) << HALF_CARRY_FLAG_BYTE_POSITION |
+        (if flag.carry      { 1 } else { 0 }) << CARRY_FLAG_BYTE_POSITION
+    } 
+}
+impl std::convert::From<u8> for FlagsRegister {
+    fn from(byte: u8) -> Self {
+        let zero = ((byte >> ZERO_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let subtract = ((byte >> SUBTRACT_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let half_carry = ((byte >> HALF_CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        let carry = ((byte >> CARRY_FLAG_BYTE_POSITION) & 0b1) != 0;
+        FlagsRegister {
+            zero,
+            subtract,
+            half_carry,
+            carry
+        } 
+    }
+}
+
+impl Registers { 
+    // Val of b bit shifted one byte right, OR with val of c to get u16 bc
+    fn get_bc(&self) -> u16 {
+        (self.b as u16) << 8
+        | self.c as u16
+    }
+
+    // AND with 0xFF00 and shift 8 bits to get first u8 of the u16 and store in b
+    // AND as u8 to get second half of the u16 and store in c
+    fn set_bc(&mut self, value: u16) {
+        self.b = ((value & 0xFF00) >> 8) as u8;
+        self.c = (value & 0xFF) as u8;
+    }
+
+    //TODO: impl other virtual 16bit regs
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
